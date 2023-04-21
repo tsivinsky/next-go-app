07556c5ad64629c80e7c64edcc3bd4d63a0da7b0
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-04-21 23:40:37 +0300

    Add static page

---
+++ app/static/page.tsx
@@ -0,0 +1,19 @@
+import { baseURL } from "../../lib/constants";
+import { Resp } from "../../types/resp";
+
+const getData = async () => {
+  const resp = await fetch(`${baseURL}/api/message`);
+  const data: Resp = await resp.json();
+
+  return data;
+};
+
+export default async function Static() {
+  const resp = await getData();
+
+  return (
+    <div>
+      <h1>{resp.message}</h1>
+    </div>
+  );
+}
